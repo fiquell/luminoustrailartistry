432fccd4878dbf892b32cf927475feff2601964a
Fiquell <fiquellh@gmail.com>
2024-08-17 19:36:16 +0800

    add basic requirements

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  bracketSameLine: true,
+  jsxSingleQuote: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
