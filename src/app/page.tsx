1e70b6efa92f59a50919ef7f88e1257a7b81104d
Fiquell <fiquellh@gmail.com>
2024-08-25 02:32:10 +0800

    fix html syntax descent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,31 @@ const Home = () => {
       <div className='container'>
         <div className='flex flex-col gap-6 font-medium lg:ml-auto lg:w-3/4 lg:flex-row'>
           <p className='lg:mt-3 lg:text-nowrap'>Smart Development</p>
-          <p className='text-2xl leading-normal lg:text-3xl'>
-            Our website is launching soon, built with cutting-edge technology
-            including{' '}
+          <div className='text-2xl lg:text-3xl'>
+            <span>
+              Our website is launching soon, built with cutting-edge technology
+              including
+            </span>
             <LinkPreview url='https://www.tailwindcss.com/'>
               Tailwind CSS
-            </LinkPreview>{' '}
-            for beautiful design,{' '}
+            </LinkPreview>
+            <span>for beautiful design,</span>
             <LinkPreview url='https://www.framer.com/motion/'>
               Framer Motion
-            </LinkPreview>{' '}
-            for smooth animations, and powered by the{' '}
-            <LinkPreview url='https://nextjs.org/'>Next.js</LinkPreview>{' '}
-            framework for top-notch performance. In the meantime, relax with a
-            llama while we finalize the details. We can&apos;t wait to show you
-            what we&apos;ve been creating. Stay tuned! Meanwhile, you can check
-            out the project on{' '}
+            </LinkPreview>
+            <span>for smooth animations, and powered by the</span>
+            <LinkPreview url='https://nextjs.org/'>Next.js</LinkPreview>
+            <span>
+              framework for top-notch performance. In the meantime, relax with a
+              llama while we finalize the details. We can&apos;t wait to show
+              you what we&apos;ve been creating. Stay tuned! Meanwhile, you can
+              check out the project on
+            </span>
             <LinkPreview url='https://github.com/fiquell/luminoustrailartistry'>
               GitHub
-            </LinkPreview>{' '}
-            for a sneak peek behind the scenes.
-          </p>
+            </LinkPreview>
+            <span>for a sneak peek behind the scenes.</span>
+          </div>
         </div>
       </div>
     </section>
